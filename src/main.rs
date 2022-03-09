968de1c0055bcba03282f3b249ef6c9a699cc748
Ubugeeei <ubuge1122@gamil.com>
2022-03-09 22:37:17 +0900

    feat: impl counter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use dioxus::prelude::*;
-
 fn main() {
     dioxus::web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
-    cx.render(rsx!{
-        div { "hello, wasm!" }
+    let (count, set_count) = use_state(&cx, || 0);
+
+    cx.render(rsx! {
+        div { "hello" }
+        p { "{count}" }
+        button { onclick: move |_| set_count(count + 1), "+1" }
     })
 }
